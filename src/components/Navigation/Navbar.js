32306a4eac256ed44743515d02b296499aa07f90
TolaMaryAnn <tolaoje2004@gmail.com>
2023-03-31 14:09:16 +0100

    servicepage

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
   const [Toggle, setToggle] = useState(false);
   return (
     <nav className="bg-white shadow-lg ">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl  mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <Link href={"/"}>
